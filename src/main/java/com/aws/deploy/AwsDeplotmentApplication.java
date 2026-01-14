dbe0c97d542ab5df7e4c84b8eccf341679b0eed2
Kevin Mora <morakevin269@gmail.com>
2026-01-14 09:34:18 -0500

    Move package declaration to the correct location

--- src/main/java/com/aws/deploy/AwsDeplotmentApplication.java
+++ src/main/java/com/aws/deploy/AwsDeplotmentApplication.java
@@ -1,4 +1,4 @@
-package com.aws.deploy.controller;
+package com.aws.deploy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
